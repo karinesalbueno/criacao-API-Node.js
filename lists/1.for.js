9d8b4e6885291a351ef522fce3d1cfaa8f6b307d
Karine <karine.costa@somagrupo.com.br>
2023-03-10 20:58:54 -0300

    API: for
    filtrando produtos e listando

---
+++ lists/1.for.js
@@ -0,0 +1,36 @@
+const service = require('./service')
+
+async function getAll() {
+  try {
+    const result = await service.AllProducts()
+    // array com o nome de todos os produtos
+    const names = []
+
+    for (index of result) {
+      names.push(index.title)
+    }
+
+    console.log(`names`, names)
+  } catch (error) {
+    console.error(`error interno`, error)
+  }
+}
+
+async function Filter() {
+  try {
+    const result = await service.FilterProduct('7')
+    // filtra um produto pelo id e retorna depois buscando a especificação
+
+    const data = result
+
+    console.log(`
+    nome: ${data.title},
+    imagem: ${data.image}
+    `)
+  } catch (error) {
+    console.error(`error interno`, error)
+  }
+}
+
+Filter()
+getAll()

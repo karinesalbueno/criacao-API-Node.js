26c1b66b69e968f5192ed9fce17b6df9020054ee
Karine <karine.costa@somagrupo.com.br>
2023-03-11 18:09:11 -0300

    nock:
    simulador de resultado

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,25 @@
 const assert = require('assert')
 const { FilterProductTest } = require('./service')
 
+// pacote nock para simular requisições
+const nock = require('nock')
+
 describe('Store Api test', function () {
+  this.beforeEach(() => {
+    const res = {
+      id: 7,
+      title: 'White Gold Plated Princess',
+      price: 9.99,
+      description: `Classic Created Wedding Engagement Solitaire Diamond Promise Ring 
+      for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...`,
+      category: 'jewelery',
+      image: 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg',
+      rating: { rate: 3, count: 400 },
+    }
+
+    nock(`https://fakestoreapi.com`).get(`/products/7`).reply(200, res)
+  })
+
   it('deve buscar o item 7 da forma correta', async () => {
     const expected = [
       {
